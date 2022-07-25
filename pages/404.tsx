085714e844b8895fd70c89431cab450b6e355d23
MongLong0214 <weplay0628@gmail.com>
2022-07-26 02:35:15 +0900

    404 완료

--- pages/404.tsx
+++ pages/404.tsx
@@ -12,10 +12,8 @@ const NotFound = () => {
   return (
     <NotFoundText>
       죄송합니다. <br />
-      요청하신 페이지를 찾을 수 없습니다. <br />
-      방문하시려는 페이지의 주소가 잘못 입력되었거나, <br />
-      페이지의 주소가 변경 혹은 삭제되어 요청하신 페이지를 찾을 수 없습니다.{" "}
       <br />
+      요청하신 페이지를 찾을 수 없습니다. <br />
       입력하신 주소가 정확한지 다시 한번 확인해 주시기 바랍니다.
       <br />
       <br />
@@ -38,4 +36,7 @@ const NotFoundText = styled.div`
   z-index: 100000;
   background-color: white;
   text-align: center;
+  @media (max-width: 1150px) {
+    font-size: 1rem;
+  }
 `;
